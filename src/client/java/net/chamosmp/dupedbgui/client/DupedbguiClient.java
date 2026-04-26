62436a3c2e3fddcec5c37ded724756be500f9c89
Chamogelastos <stavros.ssta@gmail.com>
2026-04-26 15:43:20 +0300

    Base Fabric DevKit

---
+++ src/client/java/net/chamosmp/dupedbgui/client/DupedbguiClient.java
@@ -0,0 +1,10 @@
+package net.chamosmp.dupedbgui.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DupedbguiClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/chamosmp/dupedbgui/Dupedbgui.java
@@ -0,0 +1,10 @@
+package net.chamosmp.dupedbgui;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Dupedbgui implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
